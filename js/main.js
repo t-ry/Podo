d365192bf58b083e0cda822db25f88c7f81e2eed
takahashi.r <takahashi.r299792458@gmail.com>
2022-10-09 17:06:47 +0900

    update : to responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(function($) { 
-    $('.mainvidual').bgSwitcher({
-    images: ['../images/pc_mockup.png','../images/pc_mockup_selected.png'],
-    interval: 3000,
-    loop: true,
-    });
+
+    if(window.matchMedia("(max-width: 400px)").matches){
+        $('.mainvidual').bgSwitcher(stop);
+    }else{
+        $('.mainvidual').bgSwitcher({
+        images: ['../images/pc_mockup.png','../images/pc_mockup_selected.png'],
+        interval: 5000,
+        loop: false,
+        });
+    }
 });
+
